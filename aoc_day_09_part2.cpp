59b6be244b8d1a84e7915371d39bf1129be45c06
yyhangz <y.y.hangz@hotmail.com>
2020-12-09 17:27:28 +0800

    day 9

--- aoc_day_09_part2.cpp
+++ aoc_day_09_part2.cpp
@@ -1,45 +1,48 @@
 #include "aoc.hpp"
 using namespace std;
 
-void parse_input(string fname, vector<long long> & inp_list) {
+void parse_input(string fname, vector<long long> & inp_list, queue<long long> & inp_queue) {
     ifstream in(fname);
     string str;
     while (getline(in, str)) {
         if(str.size() > 0) {
             inp_list.push_back(stoll(str));
+            inp_queue.push(stoll(str));
         }
     }
     in.close();
 }
 
-bool run_window(int start, int run_len, long long target, vector<long long> & inp_list) {
-    long long sum = 0;
-    for (int i = 0; i < run_len; i++) {
-        sum += inp_list[start + i];
-    }
-    return sum == target;
-}
-
-void find_minimax(int start, int run_len, long long & mini, long long & maxi, vector<long long> & inp_list) {
-    for (int i = 0; i < run_len; i++) {
-        mini = mini < inp_list[start + i] ? mini : inp_list[start + i];
-        maxi = maxi > inp_list[start + i] ? maxi : inp_list[start + i];
+void find_minimax(long long & mini, long long & maxi, queue<long long> & contiguous) {
+    while (contiguous.size() > 0) {
+        mini = mini < contiguous.front() ? mini : contiguous.front();
+        maxi = maxi > contiguous.front() ? maxi : contiguous.front();
+        contiguous.pop();
     }
 }
 
 int main() {
     string fname = "aoc_day_09_input.txt";
     vector<long long> inp_list;
+    queue<long long> inp_queue;
+    queue<long long> contiguous;
     const long long target = 552655238;
     long long mini = LLONG_MAX, maxi = 0;
-    parse_input(fname, inp_list);
-    for (int i = 2; i < inp_list.size(); i++) {
-        for (int j = 0; j < inp_list.size() - i; j++) {
-            if(run_window(j, i, target, inp_list)) {
-                find_minimax(j, i, mini, maxi, inp_list);
-                cout << mini + maxi << endl;
-                return 0;
-            }
+    parse_input(fname, inp_list, inp_queue);
+    long long sum = 0;
+    while(inp_queue.size() > 0) {
+        if (sum > target) {
+            sum -= contiguous.front();
+            contiguous.pop();
+        } else if (sum == target) {
+            // check min max
+            find_minimax(mini, maxi, contiguous);
+            cout << mini + maxi << endl;
+            return 0;
+        } else {
+            sum += inp_queue.front();
+            contiguous.push(inp_queue.front());
+            inp_queue.pop();
         }
     }
     return 0;
